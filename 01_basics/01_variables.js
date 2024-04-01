5d77af40c661486fa214d0b33eddedbee72bf724
tushar-khairnar <139059171+tushar-khairnar@users.noreply.github.com>
2024-04-01 20:32:32 +0000

    vairable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 44432
+let acountEmail ="tushar@google.com"
+var acountPassword = "12345"
+acountCity ="Jaypur"
+var accountState;
+
+// accountId = 2  // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
