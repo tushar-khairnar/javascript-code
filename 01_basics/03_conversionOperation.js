3dee0c758bf94b44cfb765b17c05c9e4ff8d38e1
tushar-khairnar <139059171+tushar-khairnar@users.noreply.github.com>
2024-04-07 11:39:24 +0000

    Operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,66 @@
+
+let score = "tushar"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "tushar"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "tushar" => true; or "0tushar" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(negValue + 4); // -3 + 4 = 1 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " tushar"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); // 122  //("1" + (2 + 2)) = 14
+// console.log(1 + 2 + "2"); //32
+
+// console.log( (3 + 4) * 5 % 3);
+
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
