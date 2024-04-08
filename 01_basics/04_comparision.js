2e850d71a595c67cc8c75cba8e0174a4448c6b4e
tushar-khairnar <139059171+tushar-khairnar@users.noreply.github.com>
2024-04-08 19:07:09 +0000

    compare

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strictly check with data type // "2" == 2 -true
+
+console.log("2" === 2);
